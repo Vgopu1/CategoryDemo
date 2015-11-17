05e8fde82d3135f5a1751360dc9a61a6ed30ca2e
Veena <vgopu0@walmart.com>
2015-11-16 19:04:51 -0800

    Correct version of files.

---
+++ categoryViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  categoryViewCell.swift
+//  Category
+//
+//  Created by Veena on 11/11/15.
+//  Copyright © 2015 Veena. All rights reserved.
+//
+
+import UIKit
+
+class categoryViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+}
